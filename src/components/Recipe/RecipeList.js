a412cda3df72377a236102436637bcf949043eec
David Billups <davidtbillups@gmail.com>
2021-03-18 11:16:48 -0400

    initial file structure

---
+++ src/components/Recipe/RecipeList.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import "./Kennel.css"
+import { Home } from "./Home"
+
+// import { RecipeSearch } from "./Recipe/RecipeSearch"
+// import { RecipeForm } from "./Recipe/RecipeForm"
+import { RecipeList } from "./Recipe/RecipeList"
+import { RecipeProvider } from "./Recipe/RecipeProvider"
+
+export const ApplicationViews = () => {
+    return (
+        <>
+            {/* Render the location list when http://localhost:3000/ */}
+            
+            {/* <Route exact path="/">
+                <Home />
+            </Route> */}
+
+            {/* Render the Recipe list when http://localhost:3000/recipes */}
+            <RecipeProvider>
+                <Route exact path="/recipes">
+                    <RecipeSearch />
+                    <RecipeList />
+                </Route>
+            </RecipeProvider>
+        </>
+    )
+}
